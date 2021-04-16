5ceca0f772d7d6b92e926caa41f6a2e46897e965
ziaurrehman <zia.ur.rehman@eurustechnologies.com>
2021-04-16 12:32:26 +0500

    feature commit

--- operation.py
+++ operation.py
@@ -2,4 +2,6 @@ a=10
 b=5
 print(f"sum of {a} and {b} is : {a+b}")
 
-print(f"division of {a} and {b} is : {a+b}")
+print(f"division of {a} and {b} is : {a/b}")
+
+print(f"Multiplication of {a} and {b} is : {a*b}")
